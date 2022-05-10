25e52025b3f0eaeee4be93fb4b885987bb31fd22
ja99 <janis.boersig@gmx.de>
2022-05-10 11:44:50 +0200

    working image augmentation

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+import glob
+import random
+
 from PIL import Image, ImageDraw, ImageFilter
 
-false_positive_img = Image.open('false_positive_imgs/white.png')
-positive_img = Image.open('positive_imgs/woman.png')
+N_IMAGES = 10
 
-out = false_positive_img.copy()
-out.paste(positive_img, (30,30))
-out.save("output_imgs/out.png", quality=100)
+
+false_positive_imgs = []
+for path in glob.iglob(f'false_positive_imgs/**/*.png', recursive=True):
+    false_positive_imgs.append(path)
+
+positive_imgs = []
+for path in glob.iglob(f'positive_imgs/**/*.png', recursive=True):
+    positive_imgs.append(path)
+
+
+for i in range(N_IMAGES):
+    p_i = random.randint(0, len(positive_imgs)-1)
+    positive = Image.open(positive_imgs[p_i])
+
+    f_i = random.randint(0, len(false_positive_imgs)-1)
+    false_positive = Image.open(false_positive_imgs[f_i])
+
+    if false_positive.width <= positive.width or false_positive.height <= positive.height:
+        continue
+
+    pos_x = random.randint(0, false_positive.width - positive.width)
+    pos_y = random.randint(0, false_positive.height - positive.height)
+
+    out = false_positive.copy()
+    out.paste(positive, (pos_x, pos_y))
+    out.save(f"output_imgs/{i}.png", quality=100)
